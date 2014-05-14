0a9cc211859c7f4b68ffa4c27316f508cb93400e
Nastia Shaternik <schaternik@gmail.com>
2014-05-14 17:21:17 +0300

    Set up cron task for sugarcrm updates

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,3 @@
-every 3.hours do
-  rake "sugar_crm:update"
+every 30.minutes do
+  rake "sugar_crm:update", output: { error: 'sugar_crm_update_error.log', standard: 'sugar_crm_update_cron.log' }
 end
